fff4bb15ff4b7b609c9f0a9793a337bd481a5772
Will Buckner <willb14095@gmail.com>
2022-06-02 22:29:57 -0400

    hopefully woorking the same as it was

--- bankops/Bankops.java
+++ bankops/Bankops.java
@@ -2,22 +2,26 @@ package bankops;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import account.Account;
 
 public class Bankops {
 
-  public void createAccounts(Map<String, Account> map) {
+  HashMap<String, Account> accountsMap = new HashMap<>();
+
+  public void createAccounts() {
     Account newAccount1 = new Account("Bob", "Burger", "pass1");
-    addAccountToMap(newAccount1, map);
+    addAccountToMap(newAccount1, accountsMap);
     Account newAccount2 = new Account("Stan", "Smith", "pass2");
-    addAccountToMap(newAccount2, map);
+    addAccountToMap(newAccount2, accountsMap);
     Account newAccount3 = new Account("Your", "Mom", "pass3");
-    addAccountToMap(newAccount3, map);
+    addAccountToMap(newAccount3, accountsMap);
     } 
 
-  public void addAccountToMap(Account newAccount, Map<String, Account> map){
+  private void addAccountToMap(Account newAccount, Map<String, Account> map){
     map.put(newAccount.getUsername(), newAccount);
     System.out.println("New account for " + newAccount.getFname() + " " + newAccount.getLname() + " created");
     System.out.println("Your username is " + newAccount.getUsername());
@@ -46,4 +50,52 @@ public class Bankops {
   }
     return password;
   }
+
+  public void userInterface() {
+
+    Scanner scanner = new Scanner(System.in);
+    Boolean cont = true;
+    while ( Boolean.TRUE.equals(cont)) {
+      System.out.println("Please select an option below.");
+      System.out.println("1. Create a new account");
+      System.out.println("2. Select account");
+      System.out.println("3. Exit");
+      int selection = scanner.nextInt();
+      switch (selection) {
+        case 1:
+          System.out.println("Enter your first and last name.");
+          System.out.print("First: ");
+          String fName = scanner.next();
+          System.out.print("Last: ");
+          String lName = scanner.next();
+          System.out.println("Enter a password.");
+          String password = scanner.next();
+          Account newAccount = new Account(fName, lName, password);
+          addAccountToMap(newAccount, accountsMap);
+          break;
+
+        case 2:
+          System.out.println("Enter you username and password.");
+          System.out.print("Username: ");
+          String username = scanner.next();
+          System.out.print("Password: ");
+          String pass = scanner.next();
+          Account acc = getAccount(username.toLowerCase(), pass, accountsMap);
+          if (acc != null){
+            System.out.println("Select account action.");
+          } else {
+            System.out.println("Password is wrong.");
+          }
+          break;
+    
+        default:
+          cont = false;
+          break;
+      }
+    }
+    scanner.close();
+    System.out.println("Good Bye");
+  }
+    
+  
 }
--- main/Bank.java
+++ main/Bank.java
@@ -1,62 +1,15 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.logging.Logger;
-
-import account.Account;
 import bankops.Bankops;
 
 public class Bank {
 
-static Logger logger = Logger.getLogger(Bank.class.getName());
-static HashMap<String, Account> accountsMap = new HashMap<>();
+
 static Bankops bankops = new Bankops();
   public static void main(String[] args) {
 
-    bankops.createAccounts(accountsMap);
+    bankops.createAccounts();
+    bankops.userInterface();
 
-    Scanner scanner = new Scanner(System.in);
-    Boolean cont = true;
-    while ( Boolean.TRUE.equals(cont)) {
-      System.out.println("Please select an option below.");
-      System.out.println("1. Create a new account");
-      System.out.println("2. Select account");
-      System.out.println("3. Exit");
-      int selection = scanner.nextInt();
-      switch (selection) {
-        case 1:
-          System.out.println("Enter your first and last name.");
-          System.out.print("First: ");
-          String fName = scanner.next();
-          System.out.print("Last: ");
-          String lName = scanner.next();
-          System.out.println("Enter a password.");
-          String password = scanner.next();
-          Account newAccount = new Account(fName, lName, password);
-          bankops.addAccountToMap(newAccount, accountsMap);
-          break;
-
-        case 2:
-          System.out.println("Enter you username and password.");
-          System.out.print("Username: ");
-          String username = scanner.next();
-          System.out.print("Password: ");
-          String pass = scanner.next();
-          Account acc = bankops.getAccount(username.toLowerCase(), pass, accountsMap);
-          if (acc != null){
-            System.out.println("Select account action.");
-          } else {
-            System.out.println("Password is wrong.");
-          }
-          break;
-    
-        default:
-          cont = false;
-          break;
-      }
-    }
-    scanner.close();
-    logger.info("fin");
-  }
+  }  
 }
